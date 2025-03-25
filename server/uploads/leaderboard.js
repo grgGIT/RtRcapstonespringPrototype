7867c9b9f586098be10d6793f8eea10e4582ab65
De'Andre Perry <dmp6511@rit.edu>
2025-03-25 09:20:37 -0400

    leaderboard reworked, class added

--- server/uploads/leaderboard.js
+++ server/uploads/leaderboard.js
@@ -1,31 +1,40 @@
 // leaderboardData.js
 
-// Leaderboard Model
-const leaderboardData = {
+
+// Class for GameLog
+class GameLog {
+    constructor(gameNumber, gameDate, users, winner) {
+        this.gameNumber = gameNumber;
+        this.gameDate = gameDate;
+        this.users = users;
+        this.winner = winner;
+    }
+}
+
+// Archive Model
+const gameArchive = {
     logs: [
-        {
-            gameNumber: 1,
-            gameDate: new Date('2023-10-05T14:48:00.000Z'),
-            users: [
+        new GameLog(
+            1,
+            new Date('2023-10-05T14:48:00.000Z'),
+            [
                 { name: 'John', score: 3 },
                 { name: 'Jane', score: 2 },
                 { name: 'Jack', score: 1 },
                 { name: 'Jill', score: 0 }
             ],
-            winner: { name: 'John', score: 3 }
-        },
-        {
-            gameNumber: 2,
-            gameDate: new Date('2023-10-05T14:48:00.000Z'),
-            users: [
-                { name: 'John', score: 3 },
-                { name: 'Jane', score: 2 },
-                { name: 'Jack', score: 1 },
-                { name: 'Jill', score: 0 }
-            ],
-            winner: { name: 'John', score: 3 }
-        }
+            { name: 'John', score: 3 }
+        ),
+        // Additional game logs will be added here
     ]
 };
 
-module.exports = leaderboardData;
+// Make leaderboard and rank the highest scores first
+// this will often be the winner of each game
+const topScores = {
+
+}
+
+
+// Export the leaderboard data
+module.exports = gameArchive;
